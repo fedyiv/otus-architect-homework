a0383d10e5166b0cf717746091867ef4acfcc36b
Ivan Fedyanin <ivan.a.fedyanin@gmail.com>
2020-10-23 12:33:40 +0300

    added building of docker image

---
+++ build_and_push_docker_image.sh
@@ -0,0 +1,12 @@
+# Build the app
+./gradlew clean :bootJar
+
+# Create docker image
+rm -rf docker/build
+mkdir docker/build
+cp build/libs/otus-architect-homework-1.0-SNAPSHOT.jar docker/build
+
+docker build ./docker/build -t fedyiv/otus_architect_homework_1:v1 -f docker/Dockerfile
+docker rm otus_architect_homework_1
+docker run -d -p 8080:8080 --name otus_architect_homework_1   otus_architect_homework_1
+docker push fedyiv/otus_architect_homework_1:v1
